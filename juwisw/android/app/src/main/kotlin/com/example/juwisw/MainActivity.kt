974f0d55c76f34fd1b1e6e8980e6902a29df008c
AbdullahRFA <shakibrybmn@gmail.com>
2025-09-03 21:20:09 +0600

    added just basic structure of my project

---
+++ juwisw/android/app/src/main/kotlin/com/example/juwisw/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.juwisw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ juwisw/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CampusGuardian (JU Wisdom – Mentorship & Knowledge Hub)',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'JUWise'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body:Container(
+        alignment: Alignment.center,
+        child: Text("Welcome JU Wisdom-Mentorship and Knowledge Hub ",
+        style: TextStyle(
+          fontSize: 21,
+          fontWeight: FontWeight.bold
+        ),
+        ),
+
+      )
+    );
+  }
+}
